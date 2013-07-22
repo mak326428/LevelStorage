f157c989b73061b370dceebf7463fcf0330e9703
mak326428 <mak326428@gmail.com>
2013-07-22 10:30:35 +0400

    Сornerstone for Wireless Power Synchronizers.
    I have no code right know, just idea. We'll see how I make out.
    It's wonderful to remember (when you have done everything) times when
    you had nothing. The same deal is right now. Enough babbling on, gotta
    start coding!

---
+++ makmods/levelstorage/registry/WirelessPowerSynchronizerRegistry.java
@@ -0,0 +1,16 @@
+package makmods.levelstorage.registry;
+
+/**
+ * Power syncs are the next tier of Wireless conductors
+ * While the old ones were just for transporting energy from
+ * point A to point B, this one will use frequencies (nubmers)
+ * So, there might be 50 devices in one network and they all will share power
+ * @author mak326428
+ */
+public class WirelessPowerSynchronizerRegistry {
+
+	public WirelessPowerSynchronizerRegistry() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
