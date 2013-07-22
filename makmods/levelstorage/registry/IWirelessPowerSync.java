4c0a6a2dd32e12297ce29214d3fad13e013ec31c
mak326428 <mak326428@gmail.com>
2013-07-22 11:12:37 +0400

    Basic registry & interface for power syncs

---
+++ makmods/levelstorage/registry/IWirelessPowerSync.java
@@ -0,0 +1,16 @@
+package makmods.levelstorage.registry;
+
+import net.minecraft.world.World;
+
+/**
+ * Basic interface for Wireless Power Syncs.
+ * @author mak326428
+ */
+public interface IWirelessPowerSync {
+	public int getFreq();
+	public World getWorld();
+	public int getX();
+	public int getY();
+	public int getZ();
+	public SyncType getType();
+}
