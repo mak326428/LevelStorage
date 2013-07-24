6ecc324bf073ceec8976a94c5894a0e992ba7ca5
mak326428 <mak326428@gmail.com>
2013-07-24 21:17:30 +0400

    Chargers: NBT sync complete... Much more to do.

--- makmods/levelstorage/item/ItemPortableCharger.java
+++ makmods/levelstorage/item/ItemPortableCharger.java
@@ -6,6 +6,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class ItemPortableCharger extends Item implements IInventory {
+	
+	public static final String NBT_FREQUENCY = "frequency";
 
 	public void writeNBT(ItemStack stack, PortableChargerNBTInfo info) {
 
@@ -17,6 +19,27 @@ public class ItemPortableCharger extends Item implements IInventory {
 		if (info.getChargingStack() != null) {
 			info.getChargingStack().writeToNBT(nbt);
 		}
+		
+		nbt.setInteger(NBT_FREQUENCY, info.getFrequency());
+	}
+	
+	public PortableChargerNBTInfo getNBT(ItemStack stack) {
+		if (stack.stackTagCompound == null)
+			stack.stackTagCompound = new NBTTagCompound();
+
+		NBTTagCompound nbt = stack.stackTagCompound;
+		
+		PortableChargerNBTInfo info = new PortableChargerNBTInfo();
+		
+		ItemStack stackCharging = ItemStack.loadItemStackFromNBT(nbt);
+		if (stackCharging != null)
+			info.setChargingStack(stackCharging);
+		
+		if (nbt.hasKey(NBT_FREQUENCY)) {
+			info.setFrequency(nbt.getInteger(NBT_FREQUENCY));
+		}
+		
+		return info;
 	}
 
 	public ItemPortableCharger(int par1) {
@@ -30,6 +53,11 @@ public class ItemPortableCharger extends Item implements IInventory {
 		public PortableChargerNBTInfo(ItemStack stack, int frequency) {
 			this.chargingStack = stack;
 			this.frequency = frequency;
+		}
+		
+		public PortableChargerNBTInfo() {
+			this.frequency = 0;
+			this.chargingStack = null;
 		}
 
 		public ItemStack getChargingStack() {
